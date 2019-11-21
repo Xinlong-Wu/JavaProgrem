4cf3adc759aea418f9fe5e38008a1adb6a8504ee
24472 <24472@LAPTOP-F90B3MVT>
2019-11-21 20:18:31 +0800

    个人主页面

---
+++ src/mainpane/Pane.java
@@ -0,0 +1,12 @@
+package mainpane;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Pane extends Application{
+	
+	@Override
+    public void start(Stage primaryStage) throws Exception{
+		
+	}
+}
